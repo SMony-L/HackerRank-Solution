005cba7077591c2df18dd4fec4f42a053f85479d
SMony-L <l.sovanmony@gmail.com>
2020-10-29 11:39:18 -0400

    additional warmup

---
+++ HackerRank Solution/Algorithms/Warmup/averyBigSum.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the aVeryBigSum function below.
+def aVeryBigSum(ar):
+    return sum(ar)
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
